e5da03b386a0a6dcbfde1eb62e3e12deb540a405
Brighitte Johnson <bmuses@gmail.com>
2022-01-18 09:33:45 -0800

    completed prep-javascript-arrays (fix)

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -10,5 +10,5 @@ console.log('the third element of the array is:', thirdElement);
 const length = languages.length;
 console.log('the length of the array is:', length);
 
-const lastElement = languages[5];
+const lastElement = languages[length - 1];
 console.log('the last element of the array is:', lastElement);
